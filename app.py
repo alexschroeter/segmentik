ed289d63325d0f520f651f6c82005f07de838923
Alexander Wilhelmi <me@alexanderwilhelmi.de>
2024-11-13 18:18:22 +0100

    matlab runtime and python package of semantic segmentation

--- app.py
+++ app.py
@@ -4,37 +4,50 @@ import time
 import tifffile
 import pandas as pd
 
+import SemSeg
+import matlab
+from dataclasses import dataclass
+
 @context
+@dataclass
 class MatlabContext:
     app: any
 
 @startup
-async def startup() -> MatlabContext:
-    app = matlab.initialize()
+async def startup(instance_id) -> MatlabContext:
+    app = SemSeg.initialize()
 
     return MatlabContext(app=app)
 
 
 @register
-def run_segmantik_segmentation(context: MatlabContext, n: Image) -> Table:
+def run_segmantik_segmentation(context: MatlabContext) -> Table:
     
     # TODO: Select the correct dimension of the image (dims: c, t, z, y, x)
-    image_data = n.data.sel(t=0)
+    # image_data = n.data.sel(t=0)
 
     # Convert the image data to a tiff file
-    tifffile.imwrite('image.tif', image_data)
+    # tifffile.imwrite('image.tif', image_data)
 
     # Run the segmentation algorithm
-    file_path_of_csv = context.app.run_segmentation('image.tif')
+    context.app.SemanticSegmentation(
+        "/app/input.tif",
+        "/app/results/csvFiles",
+        "Kraemer2024A-DS0008TP0005DR_D1_CH0001PL_NS",
+        matlab.double(1),
+        matlab.double(100),
+        matlab.double(0.5),
+        matlab.double(245),
+        matlab.double(1000),
+        "yes",nargout=0
+        )
 
     # Load the csv file
-    df = pd.read_csv(file_path_of_csv)
+    df = pd.read_csv("/app/results/csvFiles/semantic_segmentation_points_Kraemer2024A-DS0008TP0005DR_D1_CH0001PL_NS.csv")
 
     # Convert the csv file to a Table
-    table = from_parquet_like(df)
+    table = from_parquet_like(df, name="sem_seg_points")
 
     # TODO: Assign meaning to the columns of the table
 
     return table
-
-
